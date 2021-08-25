ab5d7b3a37c3a432ed80786fd16cd04f172cb1f3
sampanchan <spankratz@my.creativecircus.edu>
2021-08-25 15:19:31 -0400

    updated

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,2 @@
-"use strict";
-
-console.log("hello");
+console.log(" \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");var waypoint=new Waypoint({element:document.getElementById("btn-left"),handler:function(){document.getElementById("btn-left").classList.add("active")},offset:"bottom-in-view"});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 console.log(` 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+var waypoint = new Waypoint({
+    element: document.getElementById('btn-left'),
+    handler: function () {
+        let btn =document.getElementById('btn-left');
+        btn.classList.add('active');
+
+    },
+    offset: 'bottom-in-view'
+})
