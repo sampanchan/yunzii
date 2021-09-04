0d6eccc94fa10871a8d57370f9c7545a078ee8e1
sampanchan <spankratz@my.creativecircus.edu>
2021-09-04 15:18:48 -0400

    updated

--- src/js/main.js
+++ src/js/main.js
@@ -7,16 +7,150 @@ window.addEventListener('load', function() {
 });
 
 
-// var waypoint = new Waypoint({
-//     element: document.getElementById('figOne'),
-//     handler: function () {
-//         let btn =document.getElementById('figOne');
-//         btn.classList.add('active');
-//         console.log('hello world')
+var waypoint = new Waypoint({
+    element: document.getElementById('fig1'),
+    handler: function () {
+        let btn =document.getElementById('fig1');
+        btn.classList.add('active');
+        console.log('hello world')
 
-//     },
-//     offset: '95%'
-// })
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('fig2'),
+    handler: function () {
+        let btn =document.getElementById('fig2');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+
+var waypoint = new Waypoint({
+    element: document.getElementById('btn1'),
+    handler: function () {
+        let btn =document.getElementById('btn1');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('btn3'),
+    handler: function () {
+        let btn =document.getElementById('btn3');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('btn5'),
+    handler: function () {
+        let btn =document.getElementById('btn5');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('btn2'),
+    handler: function () {
+        let btn =document.getElementById('btn2');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('btn4'),
+    handler: function () {
+        let btn =document.getElementById('btn4');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('btn6'),
+    handler: function () {
+        let btn =document.getElementById('btn6');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('img1'),
+    handler: function () {
+        let btn =document.getElementById('img1');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('img3'),
+    handler: function () {
+        let btn =document.getElementById('img3');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('img5'),
+    handler: function () {
+        let btn =document.getElementById('img5');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('img2'),
+    handler: function () {
+        let btn =document.getElementById('img2');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('img4'),
+    handler: function () {
+        let btn =document.getElementById('img4');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+var waypoint = new Waypoint({
+    element: document.getElementById('img6'),
+    handler: function () {
+        let btn =document.getElementById('img6');
+        btn.classList.add('active');
+        console.log('hello world')
+
+    },
+    offset: '95%'
+})
+
+
+
 // var waypoint = new Waypoint({
 //     element: document.getElementById('btn-left'),
 //     handler: function () {
